0120046d61a9c01635b833d51a364cc00e1063ff
trc485 <thomas.chappel4@gmail.com>
2018-05-11 14:07:18 +0200

    starting template

---
+++ gulpfile.js
@@ -0,0 +1,24 @@
+const gulp   = require('gulp'),
+      csso   = require('gulp-csso'),
+      sass   = require('gulp-sass'),
+      del    = require('del');
+
+// Delete all CSS files
+gulp.task('css:clean', function() {
+  return del('public/css/*.css', { force: true });
+});
+
+// Compile CSS
+gulp.task('css:compile', ['css:clean'], function() {
+  return gulp.src('src/scss/*.scss')
+    .pipe(sass())
+    .on('error', sass.logError)
+    .pipe(csso())
+    .pipe(gulp.dest('public/css'));
+});
+
+gulp.task('build', ['css:compile']);
+
+gulp.task('develop', ['build'], function() {
+  gulp.watch('src/scss/*', ['css:compile']);
+});
